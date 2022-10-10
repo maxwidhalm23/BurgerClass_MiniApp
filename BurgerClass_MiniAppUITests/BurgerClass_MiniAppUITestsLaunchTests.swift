fc0661c680e524e7671ea04fc47970d9d8d68997
Max Widhalm <max.widhalm23@d155.org>
2022-10-10 17:20:58 -0500

    Initial Commit

---
+++ BurgerClass_MiniAppUITests/BurgerClass_MiniAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BurgerClass_MiniAppUITestsLaunchTests.swift
+//  BurgerClass_MiniAppUITests
+//
+//  Created by MAX WIDHALM on 10/10/22.
+//
+
+import XCTest
+
+class BurgerClass_MiniAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
